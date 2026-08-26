fcc9c9edf903f1c240a7dc97e516444d290fd629
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2026-08-25 22:41:53 -0300

    feat: add login form backbone

--- src/app/[locale]/(auth)/_components/login-form.tsx
+++ src/app/[locale]/(auth)/_components/login-form.tsx
@@ -1,15 +1,54 @@
+"use client"
+
+import { useTranslations } from "next-intl"
+import z from "zod"
+import {Controller, useForm} from 'react-hook-form'
+import {zodResolver} from "@hookform/resolvers/zod"
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 export default function LoginForm() {
+
+    const t = useTranslations("LoginForm")
+
+    const loginFormSchema = z.object({
+       email: z.email(`${t("EmailInputError")}`),
+       password: z.string() 
+    })
+
+    type LoginFormData = z.infer<typeof loginFormSchema>
+
+    const {control, handleSubmit} = useForm<LoginFormData>({
+        defaultValues: {
+            email: "",
+            password: ""
+        },
+        resolver: zodResolver(loginFormSchema)
+    })
+
     return (
         <form className="flex flex-col gap-6">
             <div className="flex flex-col gap-1.5">
-                {/* <Label htmlFor="email">Email</Label>
+                <Label htmlFor="email">Email</Label>
                 <Controller
                     name="email"
                     control={control}
                     render={({ field }) => (
-                        <FormInput {...field} id="email" type="email" placeholder="you@example.com" required />
+                        <Input {...field} id="email" type="email" placeholder={t("EmailPlaceholder")} required />
                     )}
-                /> */}
+                />
+            </div>
+
+            <div className="flex flex-col gap-1.5">
+                <Label htmlFor="email">{t("PasswordLabel")}</Label>
+                <Controller
+                    name="password"
+                    control={control}
+                    render={({ field }) => (
+                        <Input {...field} id="password" type="password" placeholder={t("PasswordPlaceholder")} required />
+                    )}
+                />
             </div>
 
             {/* <PasswordField control={control} /> */}
@@ -24,6 +63,7 @@ export default function LoginForm() {
             {/* <FormButton type="submit" disabled={isPending} className="w-full">
                 {isPending ? "Signing in…" : "Sign in"}
             </FormButton> */}
+            <Button type="submit" className="w-full">{t("SubmitButton")}</Button>
         </form>
     )
 }
--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -1,10 +1,18 @@
-import { useTranslations } from "next-intl"
+import { useTranslations } from "next-intl";
+import LoginForm from "../_components/login-form";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function LoginPage() {
-    const t = useTranslations("LoginPage")
-    return (
-       <div>
-        
-       </div>
-    )
+  const t = useTranslations("LoginPage");
+  return (
+    <Card className="w-full max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-xl">{t("CardTitle")}</CardTitle>
+        <CardDescription>{t("CardDescription")}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col gap-6">
+        <LoginForm/>
+      </CardContent>
+    </Card>
+  );
 }
