ca34d9ae8002e303ec9f644d9b58399a812647da
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2026-09-01 22:44:03 -0300

    chore: user account menu

--- src/app/[locale]/(admin)/_components/account-menu.tsx
+++ src/app/[locale]/(admin)/_components/account-menu.tsx
@@ -3,9 +3,15 @@
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { UserProfile } from "@/generated/prisma/client";
+import { ChevronDown } from "lucide-react";
 
 type AccountMenuProps = {
   userData: UserProfile;
@@ -14,7 +20,21 @@ type AccountMenuProps = {
 export default function AccountMenu({ userData }: AccountMenuProps) {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger render={<Button variant="outline">{userData.role}</Button>} />
+      <DropdownMenuTrigger className="flex size-10 items-center justify-center rounded-full bg-secondary text-sm font-medium text-muted-foreground">
+        {userData.name.slice(0, 1).toUpperCase()}
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-fit">
+        <DropdownMenuGroup>
+          <DropdownMenuLabel className="flex flex-col">
+            <span>{userData.name}</span>
+            <span>Perfil: {userData.role.toLowerCase()}</span>
+          </DropdownMenuLabel>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+            <button>sair</button>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
