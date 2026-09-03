2eeceeec893ddc9ec3146c4cea1f1445faf2d3d9
Paulo Vinícius Vieira Toledo <pvvtoledo@gmail.com>
2026-09-03 20:00:14 +0000

    chore: students card

--- src/app/[locale]/(admin)/students/page.tsx
+++ src/app/[locale]/(admin)/students/page.tsx
@@ -1,7 +1,32 @@
-export default function StudentsPage() {
+import { Card } from "@/components/ui/card"
+import prisma from "@/lib/prisma"
+import { getTranslations } from "next-intl/server"
+
+export default async function StudentsPage() {
+    const students = await prisma.student.findFirst({
+        include: {
+            enrollments: {
+                include: {
+                    course: true
+                }
+            }
+        }
+    })
+    const t = await getTranslations("StudentsPage")
     return (
-       <div>
-        <p>students</p>
-       </div>
+        <div className="flex flex-col gap-10 pt-4">
+            <h1 className="font-display text-2xl xl:text-3xl">{t("title")}</h1>
+            <Card className="flex flex-row w-xl items-center justify-evenly">
+
+                <div className="h-8 w-8 rounded-sm bg-red-500 "></div>
+
+                <div>
+                    <p>{students?.name}</p>
+                    {students?.enrollments.map(e => (
+                        <p key={e.id}>{e.course.name}</p>
+                    ))}
+                </div>
+            </Card>
+        </div>
     )
 }   
