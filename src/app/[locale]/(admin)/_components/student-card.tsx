1f71292eefe29614abaf9f0fb1c473d9814ef5ba
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2026-09-08 23:29:37 -0300

    fix: overdueTuition query

--- src/app/[locale]/(admin)/_components/student-card.tsx
+++ src/app/[locale]/(admin)/_components/student-card.tsx
@@ -7,51 +7,73 @@ import { calculateAge, cn } from "@/lib/utils";
 import { getTranslations } from "next-intl/server";
 
 type EnrollmentWithRelations = Prisma.EnrollmentGetPayload<{
-    include: {
-        tuitionPayments: true
-    }
-}>
+  include: {
+    tuitionPayments: true;
+  };
+}>;
 
 type StudentCardProps = {
-    studentName: string
-    studentBirthDate: Date
-    studentPhoneNumber: string | null
-    responsiblePhoneNumber: string | null
-    studentStatus: StudentStatus
-    studentEnrollments: EnrollmentWithRelations[]
-}
+  studentName: string;
+  studentBirthDate: Date;
+  studentPhoneNumber: string | null;
+  responsiblePhoneNumber: string | null;
+  studentStatus: StudentStatus;
+  studentEnrollments: EnrollmentWithRelations[];
+};
 
-export default async function StudentCard({ studentName, studentBirthDate, studentPhoneNumber, responsiblePhoneNumber, studentStatus, studentEnrollments }: StudentCardProps) {
-    const t = await getTranslations("StudentCard")
-    const studentAge = calculateAge(studentBirthDate)
+export default async function StudentCard({
+  studentName,
+  studentBirthDate,
+  studentPhoneNumber,
+  responsiblePhoneNumber,
+  studentStatus,
+  studentEnrollments,
+}: StudentCardProps) {
+  const t = await getTranslations("StudentCard");
+  const studentAge = calculateAge(studentBirthDate);
 
-    const overdueTuition = studentEnrollments.find(
-        enrollment => enrollment.tuitionPayments.length > 0 &&
-            enrollment.tuitionPayments[0].paidAt === null
-    )
+  const today = new Date();
 
-    
+  const overdueTuition = studentEnrollments.find((enrollment) =>
+    enrollment.tuitionPayments.some(
+      (tuition) => tuition.paidAt === null && tuition.dueDate < today,
+    ),
+  );
 
-    return (
-        <Card className={cn("py-4 border-l-4", studentStatus === "ACTIVE" ? "border-l-transparent" : "border-l-red-500")}>
-            <CardContent className="flex flex-row gap-3 items-center ">
-                <Avatar size="lg">
-                    <AvatarFallback>PV</AvatarFallback>
-                    <AvatarBadge className={cn(studentStatus === "ACTIVE" ? "bg-green-500" : "bg-red-500")} />
-                </Avatar>
-                <div className="flex flex-col justify-between">
-                    <p>{studentName}</p>
-                    <div className="flex gap-1">
-                        {overdueTuition && (
-                            <Badge variant="destructive">{t("overdueStatus")}</Badge>
-                        )}
-                        <span>{studentAge} {t("age")}</span>
-                        <span>•</span>
-                        <span>{studentPhoneNumber ? studentPhoneNumber : responsiblePhoneNumber}</span>
-                    </div>
-
-                </div>
-            </CardContent>
-        </Card>
-    )
+  return (
+    <Card
+      className={cn(
+        "py-4 border-l-4",
+        studentStatus === "ACTIVE"
+          ? "border-l-transparent"
+          : "border-l-red-500",
+      )}
+    >
+      <CardContent className="flex flex-row gap-3 items-center ">
+        <Avatar size="lg">
+          <AvatarFallback>PV</AvatarFallback>
+          <AvatarBadge
+            className={cn(
+              studentStatus === "ACTIVE" ? "bg-green-500" : "bg-red-500",
+            )}
+          />
+        </Avatar>
+        <div className="flex flex-col justify-between">
+          <p>{studentName}</p>
+          <div className="flex gap-1">
+            {overdueTuition && (
+              <Badge variant="destructive">{t("overdueStatus")}</Badge>
+            )}
+            <span>
+              {studentAge} {t("age")}
+            </span>
+            <span>•</span>
+            <span>
+              {studentPhoneNumber ? studentPhoneNumber : responsiblePhoneNumber}
+            </span>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
