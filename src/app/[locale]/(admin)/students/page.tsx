e52065fd675e7bb04167c5fcdf8a0db235a2263b
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2026-09-09 22:18:15 -0300

    feat: add description in students page

--- src/app/[locale]/(admin)/students/page.tsx
+++ src/app/[locale]/(admin)/students/page.tsx
@@ -1,35 +1,30 @@
-import prisma from "@/lib/prisma"
-import { getTranslations } from "next-intl/server"
-import StudentCard from "../_components/student-card"
-
-
+import prisma from "@/lib/prisma";
+import { getTranslations } from "next-intl/server";
+import StudentCard from "../_components/student-card";
 
 export default async function StudentsPage() {
-
-    const students = await prisma.student.findMany({
+  const students = await prisma.student.findMany({
+    include: {
+      enrollments: {
         include: {
-            enrollments: {
-                include: {
-                    tuitionPayments: true
-                }
-            }
-        }
-    })
+          tuitionPayments: true,
+        },
+      },
+    },
+  });
 
-    const t = await getTranslations("StudentsPage")
-    return (
-        <div className="flex flex-col gap-10 pt-4">
-            <h1 className="font-display text-2xl xl:text-3xl">{t("title")}</h1>
-            <section className="grid lg:grid-cols-2 2xl:grid-cols-3 gap-5">
-                {students.map(student => (
-                    <StudentCard
-                        key={student.id}
-                        student={student}
-                    />
-                ))}
-
-            </section>
-            
-        </div>
-    )
-}   
+  const t = await getTranslations("StudentsPage");
+  return (
+    <div className="flex flex-col gap-10 mt-8 lg:mt-12">
+      <div className="flex flex-col gap-1">
+        <h1 className="font-display text-2xl lg:text-3xl">{t("title")}</h1>
+        <h2 className="font-display text-secondary-foreground">{t("description")}</h2>
+      </div>
+      <section className="grid lg:grid-cols-2 2xl:grid-cols-3 gap-5">
+        {students.map((student) => (
+          <StudentCard key={student.id} student={student} />
+        ))}
+      </section>
+    </div>
+  );
+}
