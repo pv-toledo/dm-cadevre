77782ac59315217d9a1b795a2f3ab997bc1ed21c
Paulo Vinícius Toledo <pvvtoledo@gmail.com>
2026-09-15 22:24:57 -0300

    fix: update prisma client

--- src/app/[locale]/(admin)/students/actions.ts
+++ src/app/[locale]/(admin)/students/actions.ts
@@ -2,16 +2,20 @@
 
 import prisma from "@/lib/prisma";
 import { NewStudentFormData } from "./new/page";
+import { calculateAge, emptyToNull } from "@/lib/utils";
 
 export async function createStudent (data: NewStudentFormData) {
-    const student = await prisma.student.create({
-        data: {
-            name: data.name,
-            birthDate: data.birthDate,
-            studentPhoneNumber: data.studentPhoneNumber,
-            responsibleName: data.responsibleName,
-            responsiblePhoneNumber: data.responsiblePhoneNumber,
-            church: data.church ? data.church : 
-        }
-    })
+  const isMinor = calculateAge(data.birthDate) < 18;
+
+  const student = await prisma.student.create({
+    data: {
+      name: data.name,
+      birthDate: data.birthDate,
+      address: data.address,
+      church: emptyToNull(data.church),
+      studentPhoneNumber: isMinor ? null : emptyToNull(data.studentPhoneNumber),
+      responsibleName: isMinor ? emptyToNull(data.responsibleName) : null,
+      responsiblePhoneNumber: isMinor ? emptyToNull(data.responsiblePhoneNumber) : null,
+    },
+  });
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,3 +18,7 @@ export function getInitials(fullName: string): string {
   return `${names[0][0]}${names[names.length - 1][0]}`.toUpperCase()
 }
 
+export function emptyToNull(value?: string) {
+  const trimmed = value?.trim();
+  return trimmed ? trimmed : null;
+}
